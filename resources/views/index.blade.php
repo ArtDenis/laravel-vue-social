64a6f2b52525b8c93bde60fc65fcaac69bdef13d
ArtDenis <art.denis@mail.ru>
2022-07-03 02:14:43 +0600

    setup project

---
+++ resources/views/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang='ru'>
+<head>
+    <title>Main study page</title>
+
+    <meta name=viewport content="width=device-width, initial-scale=1">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+
+    <link href="styles/normalize.css" rel="stylesheet">
+
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+</head>
+<body>
+<div id="app">
+    <example-component></example-component>
+</div>
+
+<script src="{{mix('js/app.js')}}"></script>
+</body>
+</html>
