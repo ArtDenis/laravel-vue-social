b931c59f871ed55d85857bd04f630b7cd9bbe3bb
ArtDenis <art.denis@mail.ru>
2022-07-04 15:28:15 +0600

    start styles add

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
     <meta name=viewport content="width=device-width, initial-scale=1">
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
 
-    <link href="styles/normalize.css" rel="stylesheet">
+    <link href="/css/app.css" rel="stylesheet">
 
     <meta name="keywords" content="" />
     <meta name="description" content="" />
